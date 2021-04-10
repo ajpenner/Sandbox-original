2fbf4bc71d1a2cdd0a70e6bbe116d5523ad7779c
Andrew Penner <Andrew.Penner@domino-uk.com>
2021-04-10 03:12:05 +0000

    Add example where we define macros

---
+++ GCC/main.c
@@ -0,0 +1,10 @@
+// No includes, they may the output harder to read
+int main() 
+{
+    int i = 9;
+    while(i > 0)
+    {
+        i--;
+    }
+    return 0;
+}
---
+++ GCC/main_macro.cpp
@@ -0,0 +1,17 @@
+// No includes, they may the output harder to read
+
+#define min(X, Y)  ((X) < (Y) ? (X) : (Y))
+
+#define PI 3.14
+
+int main() 
+{
+
+    float i = PI;
+
+    float j = 2.71;
+
+    float k = min(i,j);
+
+    return 0;
+}
